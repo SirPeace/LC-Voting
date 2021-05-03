22767de2257d3a67fda178af06236dc27e86fece
SirPeace <roman.khabibulin12@gmail.com>
2021-05-03 16:51:14 +0300

    Extract DB querying logic in the IdeaRepository

--- app/Http/Livewire/IdeasIndex.php
+++ app/Http/Livewire/IdeasIndex.php
@@ -2,13 +2,10 @@
 
 namespace App\Http\Livewire;
 
-use App\Models\Category;
-use App\Models\Idea;
-use App\Models\Status;
-use App\Models\Votable;
-use Illuminate\Support\Facades\DB;
 use Livewire\Component;
+use App\Models\Category;
 use Livewire\WithPagination;
+use App\Repositories\IdeaRepository;
 
 class IdeasIndex extends Component
 {
@@ -41,47 +38,14 @@ class IdeasIndex extends Component
 
     public function render()
     {
-        $categories = Category::all();
+        $ideas = IdeaRepository::getIdeasForIndex(
+            $this->category,
+            $this->filter,
+            $this->search,
+            $this->status,
+        );
 
-        $ideas = Idea::with('user', 'category', 'status') // eager-load relationships (n+1)
-            ->when(
-                $this->status,
-                function ($query) {
-                    $statuses = Status::pluck('id', 'name');
-                    return $query->where('status_id', $statuses[$this->status]);
-                }
-            )
-            ->when(
-                $this->category,
-                function ($query) use ($categories) {
-                    $categories = $categories->pluck('id', 'name');
-                    return $query->where('category_id', $categories[$this->category]);
-                }
-            )
-            ->when(
-                $this->filter,
-                function ($query) {
-                    if ($this->filter === 'top_voted') {
-                        return $query->orderByDesc('votes_count');
-                    }
-
-                    if ($this->filter === 'user_ideas') {
-                        return $query->where('user_id', auth()->id());
-                    }
-                }
-            )
-            ->when(
-                mb_strlen($this->search) >= 3,
-                fn ($query) => $query->where('title', 'ilike', "%$this->search%")
-            )
-            ->addSelect([ // check if user voted for idea (n+1)
-                'voted_by_user' => Votable::select('id')
-                    ->where('user_id', auth()->id())
-                    ->whereColumn('votable_id', 'ideas.id')
-            ])
-            ->withCount('votes') // get votes count (n+1)
-            ->latest('id')
-            ->simplePaginate(Idea::PAGINATION_COUNT);
+        $categories = Category::all()->toBase();
 
         return view('livewire.ideas-index', compact('ideas', 'categories'));
     }
---
+++ app/Repositories/IdeaRepository.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Models\Idea;
+use App\Models\Status;
+use App\Models\Votable;
+use App\Models\Category;
+use Illuminate\Pagination\Paginator;
+
+class IdeaRepository
+{
+    /**
+     * Get paginated list of Idea models to render on index page
+     *
+     * @param string $category Category name query string
+     * @param string $filter Filter option query string
+     * @param string $search Search value query string
+     * @param string $status Status name query string
+     * @return Illuminate\Pagination\Paginator
+     */
+    static public function getIdeasForIndex(
+        string $category = '',
+        string $filter = '',
+        string $search = '',
+        string $status = '',
+    ): Paginator {
+        $categories = Category::all()->toBase();
+
+        $ideasPaginator = Idea::with('user', 'category', 'status') // eager-load relationships (n+1)
+            ->when(
+                $status,
+                function ($query) use ($status) {
+                    $statuses = Status::pluck('id', 'name');
+                    return $query->where('status_id', $statuses[$status]);
+                }
+            )
+            ->when(
+                $category,
+                function ($query) use ($categories, $category) {
+                    $categories = $categories->pluck('id', 'name');
+                    return $query->where('category_id', $categories[$category]);
+                }
+            )
+            ->when(
+                $filter,
+                function ($query) use ($filter) {
+                    if ($filter === 'top_voted') {
+                        return $query->orderByDesc('votes_count');
+                    }
+
+                    if ($filter === 'user_ideas') {
+                        return $query->where('user_id', auth()->id());
+                    }
+                }
+            )
+            ->when(
+                mb_strlen($search) >= 3,
+                fn ($query) => $query->where('title', 'ilike', "%$search%")
+            )
+            ->addSelect([ // check if user voted for idea (n+1)
+                'voted_by_user' => Votable::select('id')
+                    ->where('user_id', auth()->id())
+                    ->whereColumn('votable_id', 'ideas.id')
+            ])
+            ->withCount('votes') // get votes count (n+1)
+            ->latest('id')
+            ->simplePaginate(Idea::PAGINATION_COUNT);
+
+        return $ideasPaginator;
+    }
+}
