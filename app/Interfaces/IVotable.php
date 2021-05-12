8ed377f5186e86ee69a6fe73e3f92f981d7611a9
SirPeace <roman.khabibulin12@gmail.com>
2021-05-12 13:56:50 +0300

    Update the phpDOC for IVotable

--- app/Interfaces/IVotable.php
+++ app/Interfaces/IVotable.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Relations\MorphToMany;
 interface IVotable
 {
     /**
+     * User can vote for this instance. \
      * MorphToMany relationship with User model on 'votables' table
      *
      * @return Illuminate\Database\Eloquent\Relations\MorphToMany<App\Models\User, 'votable'>
