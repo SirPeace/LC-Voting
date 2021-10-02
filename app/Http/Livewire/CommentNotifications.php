81fb99ca7f7205198a47594480f0fa33867c503d
SirPeace <roman.khabibulin12@gmail.com>
2021-10-02 17:57:32 +0300

    New features
    - Limit the count of notifications to display
    - Add a skeleton loader

--- app/Http/Livewire/CommentNotifications.php
+++ app/Http/Livewire/CommentNotifications.php
@@ -6,13 +6,35 @@ use Livewire\Component;
 
 class CommentNotifications extends Component
 {
-    public $notifications = [];
+    const NOTIFICATIONS_THRESHOLD = 10;
+
+    public $notifications;
+    public $notificationsCount;
+    public $isLoading = true;
 
     protected $listeners = ['getNotifications'];
 
     public function getNotifications(): void
     {
-        $this->notifications = auth()->user()->unreadNotifications;
+        $this->notifications = optional(auth()->user())
+            ->unreadNotifications()
+            ->latest()
+            ->take(self::NOTIFICATIONS_THRESHOLD)
+            ->get();
+
+        $this->isLoading = false;
+    }
+
+    public function mount()
+    {
+        $this->notifications = collect([]);
+        $this->notificationsCount = optional(auth()->user())
+            ->unreadNotifications()
+            ->count();
+
+        if ($this->notificationsCount > self::NOTIFICATIONS_THRESHOLD) {
+            $this->notificationsCount = self::NOTIFICATIONS_THRESHOLD."+";
+        }
     }
 
     public function render()
